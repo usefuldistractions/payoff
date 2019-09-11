7c4c56701c4f231a4a06aab41b114387167accdc
JP Cutter <jp@usefuldistractions.com>
2019-09-11 03:37:30 -0700

    Added basic unit tests for all public properties

--- app/spec/PayoffSpec.js
+++ app/spec/PayoffSpec.js
@@ -1,9 +1,9 @@
-describe("Payoff javascript library", function() {
-    it("exists.", function() {
+describe('Payoff javascript library', function() {
+    it('exists.', function() {
         expect(Payoff).not.toBe(jasmine.any(Function));
     });
 
-    describe("payoff instances", function() {
+    describe('payoff instances', function() {
         var payoff;
 
         payoff = new Payoff({
@@ -14,17 +14,53 @@ describe("Payoff javascript library", function() {
             startYear: 2018
         });
 
-        it("constructs successfully.", function() {
+        it('constructs successfully.', function() {
             expect(payoff).toEqual(jasmine.any(Object));
         });
 
-        it("has a 'monthlyPayment' property.", function() {
+        it('has an "amount" property.', function() {
+            expect(payoff.amount).toEqual(jasmine.any(Number));
+        });
+        
+        it('has an "interestPaid" property.', function() {
+            expect(payoff.interestPaid).toEqual(jasmine.any(Number));
+        });
+
+        it('has a "startMonth" property.', function() {
+            expect(payoff.startMonth).toEqual(jasmine.any(Number));
+        });
+
+        it('has a "startYear" property.', function() {
+            expect(payoff.startYear).toEqual(jasmine.any(Number));
+        });
+
+        it('has a "monthlyPayment" property.', function() {
             var monthlyPayment = payoff.monthlyPayment;
             expect(monthlyPayment).toEqual(jasmine.any(Number));
             expect(monthlyPayment > 0).toBe(true);
         });
 
-        it("generates an amortization 'table'.", function() {
+        it('has a "paid" property.', function() {
+            expect(payoff.paid).toEqual(jasmine.any(Number));
+        });
+
+        it('has a "payoffDate" property.', function() {
+            expect(payoff.payoffDate).toEqual(jasmine.any(String));
+        });
+
+        it('has a "payoffMonth" property.', function() {
+            expect(payoff.payoffMonth).toEqual(jasmine.any(Number));
+        });
+
+        it('has a "payoffYear" property.', function() {
+            expect(payoff.payoffYear).toEqual(jasmine.any(Number));
+        });
+
+        it('has a "principalPaid" property.', function() {
+            expect(payoff.principalPaid).toEqual(jasmine.any(Number));
+        });
+
+        it('generates an amortization "table".', function() {
             expect(payoff.table).toEqual(jasmine.any(Array));
         });
     });
