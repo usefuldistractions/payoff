8d56dcc7b61d591d57801622939cddce8fe3387e
JP Cutter <jp@usefuldistractions.com>
2019-10-28 02:54:09 -0700

    Updated reconcile() to be private per instance

--- src/Payoff.js
+++ src/Payoff.js
@@ -19,159 +19,158 @@
         let terms = { }; // contains the terms of the loan
         let index = 0; // an incremented index, to be assigned to additional payments
 
-        Object.defineProperties(this, {
-            reconcile: {
-                value: () => {
-                    const {
-                        startMonth, // the month the loan starts; NOTE: (month + 1) for first payment date, -1 for JS zero-indexing
-                        startYear, // the year the loan starts
-                        duration, // total number of months in the loan terms
-                        amount, // total amount borrowed
-                        rate // the vig (apr)
-                    } = terms;
-            
-                    const startDay = 1; // hard-code start-day to 1
-            
-                    const monthlyRate = rate / 12;
-                    const totalRate = monthlyRate + 1;
-                    const overallRate = Math.pow(totalRate, duration);
-            
-                    let monthlyPayment = amount * ((monthlyRate * overallRate) / (overallRate - 1));
-                    monthlyPayment = roundNumber(monthlyPayment, 2);
-            
-                    let [total, totalInterest, totalPrincipal] = [0, 0, 0];
-                    let principal = amount;
-                    let table = [ ];
-            
-                    for(let i = 0; i <= duration; i++) {
-                        const date = new Date(startYear, startMonth + i, startDay);
-                        const year = date.getFullYear();
-                        const yearLabel = year.toString();
-                        const month = date.getMonth()+1;
-                        const monthLabel = padZeroes(month, 2);
-                        const day = date.getDate();
-                        const dayLabel = padZeroes(day, 2);
-                        let extraPayment = 0;
+        // private function
+        const reconcile = () => {
+            const {
+                startMonth, // the month the loan starts; NOTE: (month + 1) for first payment date, -1 for JS zero-indexing
+                startYear, // the year the loan starts
+                duration, // total number of months in the loan terms
+                amount, // total amount borrowed
+                rate // the vig (apr)
+            } = terms;
+    
+            const startDay = 1; // hard-code start-day to 1
+    
+            const monthlyRate = rate / 12;
+            const totalRate = monthlyRate + 1;
+            const overallRate = Math.pow(totalRate, duration);
+    
+            let monthlyPayment = amount * ((monthlyRate * overallRate) / (overallRate - 1));
+            monthlyPayment = roundNumber(monthlyPayment, 2);
+    
+            let [total, totalInterest, totalPrincipal] = [0, 0, 0];
+            let principal = amount;
+            let table = [ ];
+    
+            for(let i = 0; i <= duration; i++) {
+                const date = new Date(startYear, startMonth + i, startDay);
+                const year = date.getFullYear();
+                const yearLabel = year.toString();
+                const month = date.getMonth()+1;
+                const monthLabel = padZeroes(month, 2);
+                const day = date.getDate();
+                const dayLabel = padZeroes(day, 2);
+                let extraPayment = 0;
 
-                        for(let j = 0; j < payments.length; j++) {
-                            const payment = payments[j];
-                            const paymentYear = payment.year;
-                            const paymentMonth = payment.month;
-                            const paymentEndYear = payment.endYear;
-                            const paymentEndMonth = payment.endMonth;
-                            const recurring = payment.recurring;
-                            let matches = false;
+                for(let j = 0; j < payments.length; j++) {
+                    const payment = payments[j];
+                    const paymentYear = payment.year;
+                    const paymentMonth = payment.month;
+                    const paymentEndYear = payment.endYear;
+                    const paymentEndMonth = payment.endMonth;
+                    const recurring = payment.recurring;
+                    let matches = false;
 
-                            if(paymentYear === year && paymentMonth === month) { // exact match
-                                matches = true;
-                            } else if(recurring) { // monthly range of extra payments
-                                if(year > paymentYear || (year === paymentYear && month >= paymentMonth)) { // range has started
-                                    if(typeof paymentEndMonth === `number` && typeof paymentEndYear === `number`) { // end date was passed
-                                        if(year < paymentEndYear || (year === paymentEndYear && month <= paymentEndMonth)) { // range hasn't ended
-                                            matches = true;
-                                        }
-                                    } else { // no recurring end date set
-                                        matches = true;
-                                    }
+                    if(paymentYear === year && paymentMonth === month) { // exact match
+                        matches = true;
+                    } else if(recurring) { // monthly range of extra payments
+                        if(year > paymentYear || (year === paymentYear && month >= paymentMonth)) { // range has started
+                            if(typeof paymentEndMonth === `number` && typeof paymentEndYear === `number`) { // end date was passed
+                                if(year < paymentEndYear || (year === paymentEndYear && month <= paymentEndMonth)) { // range hasn't ended
+                                    matches = true;
                                 }
+                            } else { // no recurring end date set
+                                matches = true;
                             }
-
-                            if(matches) {
-                                extraPayment += payment.amount;
-                            }
-                        }
-            
-                        const interestPaid = roundNumber(principal * monthlyRate, 2);
-                        let principalPaid = roundNumber(monthlyPayment - interestPaid + extraPayment, 2);
-                        
-                        principal = roundNumber(principal - principalPaid, 2);
-                        if(principal < 0) {
-                            principalPaid += principal;
-                            principal = 0;
-                        }
-
-                        const paid = interestPaid + principalPaid;
-
-                        table.push({
-                            paid,
-                            year,
-                            month,
-                            principal,
-                            interestPaid,
-                            principalPaid,
-                            date: `${yearLabel}-${monthLabel}-${dayLabel}`,
-                        });
-
-                        total += paid;
-                        totalInterest += interestPaid;
-                        totalPrincipal += principalPaid;
-
-                        if(principal <= 0) {
-                            break;
                         }
                     }
-            
-                    let lastPayment = table[table.length - 1];
-            
-                    /**
-                     * The initial loan amount.
-                     * @member payment#amount
-                     * @type {number}
-                     **/
-                    this.amount = roundNumber(amount, 2);
-                    /**
-                     * The total interest paid over the course of the loan.
-                     * @type {number}
-                     **/
-                    this.interestPaid = roundNumber(totalInterest, 2);
-                    /** 
-                     * The minimum monthly payment in order to pay off the loan on time.
-                     * @type {number}
-                     **/
-                    this.monthlyPayment = roundNumber(monthlyPayment, 2);
-                    /**
-                     * The total amount paid over the course of the loan.
-                     * @type {number}
-                     **/
-                    this.paid = roundNumber(total, 2);
-                    /**
-                     * The date of the final payment.
-                     * @type {string}
-                     **/
-                    this.payoffDate = lastPayment.date;
-                    /**
-                     * The month of the final payment.
-                     * @type {number}
-                     **/
-                    this.payoffMonth = lastPayment.month;
-                    /**
-                     * The year of the final payment.
-                     * @type {number}
-                     **/
-                    this.payoffYear = lastPayment.year;
-                    /**
-                     * The total principal paid over the course of the loan.
-                     * @type {number}
-                     **/
-                    this.principalPaid = roundNumber(totalPrincipal, 2);
-                    /**
-                     * The start year of the loan.
-                     * @type {number}
-                     **/
-                    this.startYear = startYear;
-                    /**
-                     * The start month of the loan.
-                     * @type {number}
-                     **/
-                    this.startMonth = startMonth;
-                    /**
-                     * The amortization table of each payment.
-                     * @type {array}
-                     **/
-                    this.table = table;
-                }
-            },
 
+                    if(matches) {
+                        extraPayment += payment.amount;
+                    }
+                }
+    
+                const interestPaid = roundNumber(principal * monthlyRate, 2);
+                let principalPaid = roundNumber(monthlyPayment - interestPaid + extraPayment, 2);
+                
+                principal = roundNumber(principal - principalPaid, 2);
+                if(principal < 0) {
+                    principalPaid += principal;
+                    principal = 0;
+                }
+
+                const paid = interestPaid + principalPaid;
+
+                table.push({
+                    paid,
+                    year,
+                    month,
+                    principal,
+                    interestPaid,
+                    principalPaid,
+                    date: `${yearLabel}-${monthLabel}-${dayLabel}`,
+                });
+
+                total += paid;
+                totalInterest += interestPaid;
+                totalPrincipal += principalPaid;
+
+                if(principal <= 0) {
+                    break;
+                }
+            }
+    
+            let lastPayment = table[table.length - 1];
+    
+            /**
+             * The initial loan amount.
+             * @member payment#amount
+             * @type {number}
+             **/
+            this.amount = roundNumber(amount, 2);
+            /**
+             * The total interest paid over the course of the loan.
+             * @type {number}
+             **/
+            this.interestPaid = roundNumber(totalInterest, 2);
+            /** 
+             * The minimum monthly payment in order to pay off the loan on time.
+             * @type {number}
+             **/
+            this.monthlyPayment = roundNumber(monthlyPayment, 2);
+            /**
+             * The total amount paid over the course of the loan.
+             * @type {number}
+             **/
+            this.paid = roundNumber(total, 2);
+            /**
+             * The date of the final payment.
+             * @type {string}
+             **/
+            this.payoffDate = lastPayment.date;
+            /**
+             * The month of the final payment.
+             * @type {number}
+             **/
+            this.payoffMonth = lastPayment.month;
+            /**
+             * The year of the final payment.
+             * @type {number}
+             **/
+            this.payoffYear = lastPayment.year;
+            /**
+             * The total principal paid over the course of the loan.
+             * @type {number}
+             **/
+            this.principalPaid = roundNumber(totalPrincipal, 2);
+            /**
+             * The start year of the loan.
+             * @type {number}
+             **/
+            this.startYear = startYear;
+            /**
+             * The start month of the loan.
+             * @type {number}
+             **/
+            this.startMonth = startMonth;
+            /**
+             * The amortization table of each payment.
+             * @type {array}
+             **/
+            this.table = table;
+        };
+
+        Object.defineProperties(this, {
             /**
              * Recalculate the payoff properties and amortization table.
              * @method Payoff#update
@@ -189,7 +188,7 @@
                     }
 
                     terms = newTerms;
-                    return this.reconcile();
+                    return reconcile();
                 }
             },
 
@@ -225,7 +224,7 @@
                     }
 
                     payments.push(payment);
-                    return this.reconcile();
+                    return reconcile();
                 }
             },
 
@@ -248,7 +247,7 @@
                             break;
                         }
                     }
-                    return this.reconcile();
+                    return reconcile();
                 }
             }
         });
